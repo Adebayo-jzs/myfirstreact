8c5b60daac475c70163d31a362791119ddd5c07c
Adebayo-jzs <adedejiadebayo732@gmail.com>
2024-12-28 05:47:47 +0100

    Initial commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './header.jsx'
+import Student from './student.jsx';
+
+function App() {
+    return(
+        <>
+        <Header/>
+        <Student name="Sam" age={17}/>
+        <Student name="Sam" age={17}/>
+        </>
+  );
+}
+
+export default App
